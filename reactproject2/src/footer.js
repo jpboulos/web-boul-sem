8e0a9ce3d2e4076195dc5714c5b8fb810d54326f
JoeSema <116891983+JoeSema@users.noreply.github.com>
2022-10-28 19:25:39 +0300

    Update footer.js

--- reactproject2/src/footer.js
+++ reactproject2/src/footer.js
@@ -1,7 +1,52 @@
-const Footer = () => {
-    const year = new Date().getFullYear();
+import React from "react";
+import "./Footer.css";
 
-    return <footer>{`Copyright © YALLA.INC ${year}`}</footer>;
+
+const Footer = () => {
+    return (
+        <div className="main-footer">
+            <div className="container">
+                <div className="row">
+                    {/*Column1 */}
+                    <div className="col">
+                        <h4>Legal</h4>
+                        <ul className="list-unstyled">
+                            <li>safetyy</li>
+                            <li>jp</li>
+                            <li>joe</li>
+                        </ul>
+                    </div>
+                     {/*Column2 */}
+                     <div className="col">
+                        <h4>Careers</h4>
+                        <ul className="list-unstyled">
+                            <li>safetyy</li>
+                            <li>jp</li>
+                            <li>joe</li>
+                        </ul>
+                    </div>
+                    {/*Column3 */}
+                    <div className="col">
+                        <h4>Social</h4>
+                        <ul className="list-unstyled">
+                            <li>safetyy</li>
+                            <li>jp</li>
+                            <li>joe</li>
+                        </ul>
+                    </div>
+                </div>
+                <hr/>
+                <div className="row">
+                    <p className="col-sm">
+                        &copy;{new Date().getFullYear()} YALLA INC | All right reserved | Terms Of Service | Privacy
+                    </p>
+
+                </div>
+
+            </div>
+         
+        </div>
+    );
 };
 
 export default Footer;
