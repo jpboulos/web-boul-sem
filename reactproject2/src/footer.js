d5f190d7d2a4bc0ad0a0118386199d0520d7cef3
jpboulos <jpb812001@gmail.com>
2022-10-31 17:11:30 +0200

    footer1.1

--- reactproject2/src/footer.js
+++ reactproject2/src/footer.js
@@ -3,9 +3,50 @@ import "./Footer.css";
 
 
 const Footer = () => {
-    const year = new Date().getFullYear();
+    return (
+        <div className="main-footer">
+            <div className="container">
+                <div className="row">
+                    {/*Column1 */}
+                    <div className="col">
+                        <h4>Legal</h4>
+                        <ul className="list-unstyled">
+                            <li>safetyy</li>
+                            <li>jp</li>
+                            <li>joe</li>
+                        </ul>
+                    </div>
+                    {/*Column2 */}
+                    <div className="col">
+                        <h4>Careers</h4>
+                        <ul className="list-unstyled">
+                            <li>safetyy</li>
+                            <li>jp</li>
+                            <li>joe</li>
+                        </ul>
+                    </div>
+                    {/*Column3 */}
+                    <div className="col">
+                        <h4>Social</h4>
+                        <ul className="list-unstyled">
+                            <li>safetyy</li>
+                            <li>jp</li>
+                            <li>joe</li>
+                        </ul>
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <p className="col-sm">
+                        &copy;{new Date().getFullYear()} YALLA INC | All right reserved | Terms Of Service | Privacy
+                    </p>
 
-    return <footer>{`Copyright © YALLA.INC ${year}`}</footer>;
+                </div>
+
+            </div>
+
+        </div>
+    );
 };
 
 export default Footer;
